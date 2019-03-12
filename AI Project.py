c79c9240dee699727ff899506e7856d09e897ca8
DonKerubino <30708347+DonKerubino@users.noreply.github.com>
2019-03-12 16:06:10 +0000

    Add files via upload

---
+++ AI Project.py
@@ -0,0 +1,50 @@
+import snap
+import time as timetime
+
+####reads the data.csv file and returns a network contained within it
+
+def importGraph():
+    dataFile = open("data.csv", "r")
+    g = snap.TNEANet.New()
+    while True:
+        data = dataFile.readline().split(",")
+        data[len(data) - 1] = data[len(data) - 1].replace("\n", "")
+        if(data == ['']):
+            break
+        source = int(data[0])
+        target = int(data[1])
+        rating = int(data[2])
+        time = float(data[3])
+
+    ##    print(source, target, rating, time)
+        if not (g.IsNode(source)):
+            g.AddNode(source)
+        if not (g.IsNode(target)):
+            g.AddNode(target)
+        g.AddEdge(source, target)
+        
+        g.AddIntAttrDatE(g.GetEI(source, target).GetId(), rating, "rating")
+        g.AddFltAttrDatE(g.GetEI(source, target).GetId(), time, "time")
+    dataFile.close()
+    return g
+
+
+
+graph = importGraph()
+
+print("Nodes: " + str(graph.GetNodes()) + "\t" + "Edges: " + str(graph.GetEdges()) + "\n")
+for edge in graph.Edges():
+    print("Edge ID: " + str(edge.GetId()))
+    print("Source Node: " + str(edge.GetSrcNId()) + " --> Target Node: " + str(edge.GetDstNId()))
+    print("Rating: " + str(graph.GetIntAttrDatE(edge.GetId(), "rating")))
+    print("Time: " + str(graph.GetFltAttrDatE(edge.GetId(), "time")))
+    print
+    timetime.sleep(1)
+
+
+####creates a .dot file that idk what to do with
+##snap.DrawGViz(graph, snap.gvlDot, "graph.png", "graph")
+
+
+####if gnuplot is installed, creates a .plt file that when run creates a plot png
+snap.PlotInDegDistr(graph, "Plot", "Descriptiom of the plot")
