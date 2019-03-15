5d1bc908d975f89ceedea7213ad1ed30c688a8a2
dkblackley <44552944+dkblackley@users.noreply.github.com>
2019-03-15 18:50:38 +0000

    Added in thresholding, as well as commenting up a bunch of things and implementing Object Orientated stuff

--- AI Project.py
+++ AI Project.py
@@ -1,11 +1,68 @@
 import snap
 import snapPlot
-import time as timetime
+import datetime
+import time
+
+"""
+Following class deals with manipulating the snap graph, It has methods to threshold and cycle through
+each node in the graph
+"""
+
+class snapGraph(object):
+
+    #Constructor
+    def __init__(self, network):
+        self.network = network
+
+    #Cycles through each node in the snap graph and displays its information
+    def cycleThroughNodes(self):
+
+
+        print("Nodes: " + str(self.network.GetNodes()) + "\t" + "Edges: " + str(self.network.GetEdges()) + "\n")
+        for edge in self.network.Edges():
+            print("Edge ID: " + str(edge.GetId()))
+            print("Source Node: " + str(edge.GetSrcNId()) + " --> Target Node: " + str(edge.GetDstNId()))
+            print("Rating: " + str(self.network.GetIntAttrDatE(edge.GetId(), "rating")))
+
+            #convert date to epoch time using the datetime python library
+            ts_epoch = self.network.GetFltAttrDatE(edge.GetId(), "time")
+            print("Time: " + datetime.datetime.fromtimestamp(ts_epoch).strftime('%Y-%m-%d'))
+            time.sleep(1)
+
+    """
+    This function takes in a minimum date and a maximum date, then thresholds the network based on those dates. If no
+    maximum date is provided then the default is the first day of 2019, as no edges should have a time above this value
+    The function takes the date in the form of a string, where the string has a pattern of DD.MM.YYYY, such as 17.03.2018
+    would relate to the 17th of March 2018
+    """
+    def thresholdNetwork(self, min, max='01.01.2019'):
+
+        import time
+        pattern = '%d.%m.%Y'
+
+        minEpoch = time.mktime(time.strptime(min, pattern))
+        maxEpoch = time.mktime(time.strptime(max, pattern))
+
+        #Removes edges not within the time frame
+        for EI in self.network.Edges():
+            time = self.network.GetFltAttrDatE(EI, "time")
+            if time < minEpoch or time > maxEpoch:
+                self.network.DelEdge(EI.GetSrcNId(), EI.GetDstNId())
+
+        #Removes Nodes with No Edges
+        for NI in self.network.Nodes():
+            if NI.GetInDeg() == 0 and NI.GetOutDeg() == 0:
+                self.network.DelNode(NI.GetId())
+
+    #Return the graph
+    def returnNetwork(self):
+        return self.network
+
 
 ####reads the data.csv file and returns a network contained within it
 
 def importGraph():
-    dataFile = open("data.csv", "r")
+    dataFile = open("soc-sign-bitcoinotc.csv", "r")
     g = snap.TNEANet.New()
     while True:
         data = dataFile.readline().split(",")
@@ -31,36 +88,24 @@ def importGraph():
 
 
 
-graph = importGraph()
-
-plot = snapPlot.plotNet(graph)
-plot.plotGraph()
 
 
 
-"""
-labels = snap.TIntStrH()
-for NI in graph.Nodes():
-    labels[NI.GetId()] = str(NI.GetId())
-snap.DrawGViz(graph, snap.gvlDot, "output.png", " ", labels)
-"""
+importedGraph = importGraph()
+
+network = snapGraph(importedGraph)
+
+#network.cycleThroughNodes()
+
+network.thresholdNetwork('17.07.2015', '20.01.2016')
 
 
-"""
-print("Nodes: " + str(graph.GetNodes()) + "\t" + "Edges: " + str(graph.GetEdges()) + "\n")
-for edge in graph.Edges():
-    print("Edge ID: " + str(edge.GetId()))
-    print("Source Node: " + str(edge.GetSrcNId()) + " --> Target Node: " + str(edge.GetDstNId()))
-    print("Rating: " + str(graph.GetIntAttrDatE(edge.GetId(), "rating")))
-    print("Time: " + str(graph.GetFltAttrDatE(edge.GetId(), "time")))
-    print
-    timetime.sleep(1)
-"""
+plot = snapPlot.plotNet(network)
+plot.draw()
 
+#Below here are the other types of plots you can plot
+#plot.draw("spect")
+#plot.draw("c")
+#plot.draw("fr")
+#plot.draw("sh")
 
-####creates a .dot file that idk what to do with
-##snap.DrawGViz(graph, snap.gvlDot, "graph.png", "graph")
-
-
-####if gnuplot is installed, creates a .plt file that when run creates a plot png
-#snap.PlotInDegDistr(graph, "Plot", "Descriptiom of the plot")
--- snapPlot.py
+++ snapPlot.py
@@ -6,7 +6,9 @@ class plotNet(object):
 
     def __init__(self, network):
 
-        new_net = nx.Graph()
+        network = network.returnNetwork()
+
+        new_net = nx.DiGraph()
         for NI in network.Nodes():
 
             ID = NI.GetId()
@@ -23,14 +25,63 @@ class plotNet(object):
 
         self.network = new_net
 
-        #print nx.info(new_net)
+        print nx.info(new_net)
 
-    def plotGraph(self):
+    def getNetworkx(self):
 
-        sp = nx.spring_layout(self.network)
+        return self.network
 
+    def determineGraphType(self, type):
+
+        if type == "sp":
+            return nx.spring_layout(self.network)
+
+        elif type == "spect":
+            return nx.spectral_layout(self.network)
+
+        elif type == "sh":
+            return nx.shell_layout(self.network)
+
+        elif type == "fr":
+            return nx.fruchterman_reingold_layout(self.network)
+
+        elif type == "c":
+            return nx.circular_layout(self.network)
+
+
+
+    def draw(self, type="sp"):
+
+        drawType = self.determineGraphType(type)
         plt.axis('off')
-
-        nx.draw_networkx(self.network, pos=sp, with_labels=False, node_size=15, edge_size=1)
-
+        nx.draw_networkx(self.network, pos=drawType, with_labels=False, node_size=35)
         plt.show()
+"""
+    def drawSpect(self):
+
+        sp = nx.spectral_layout(self.network)
+        plt.axis('off')
+        nx.draw_networkx(self.network, pos=sp, with_labels=False, node_size=15)
+        plt.show()
+
+    def drawSH(self):
+
+        sh = nx.shell_layout(self.network)
+        plt.axis('off')
+        nx.draw_networkx(self.network, pos=sh, with_labels=False, node_size=15)
+        plt.show()
+
+    def drawC(self):
+
+        c = nx.circular_layout(self.network)
+        plt.axis('off')
+        nx.draw_networkx(self.network, pos=c, with_labels=False, node_size=15)
+        plt.show()
+
+    def drawFR(self):
+
+        fr = nx.fruchterman_reingold_layout(self.network)
+        plt.axis('off')
+        nx.draw_networkx(self.network, pos=fr, with_labels=False, node_size=15)
+        plt.show()
+"""
